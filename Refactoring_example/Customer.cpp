fde058e16a7c460b466d7d6702cce7f8830de599
yamileva <a.yamileva@ugatu.su>
2022-11-03 09:30:20 +0500

    Рефакторинг: переименование локальных переменных в Customer::amountFor

--- Refactoring_example/Customer.cpp
+++ Refactoring_example/Customer.cpp
@@ -30,26 +30,26 @@ string Customer::statement() {
 	return result;
 }
 
-double Customer::amountFor(Rental each)
+double Customer::amountFor(Rental aRental)
 {
-	double thisAmount = 0;
-	switch (each.getMovie().getPriceCode()) {
+	double result = 0;
+	switch (aRental.getMovie().getPriceCode()) {
 	case Movie::REGULAR:
-		thisAmount += 2;
+		result += 2;
 
-		if (each.getDaysRented() > 2)
-			thisAmount += (each.getDaysRented() - 2) * 1.5;
+		if (aRental.getDaysRented() > 2)
+			result += (aRental.getDaysRented() - 2) * 1.5;
 		break;
 
 	case Movie::NEW_RELEASE:
-		thisAmount += each.getDaysRented() * 3;
+		result += aRental.getDaysRented() * 3;
 		break;
 
 	case Movie::CHILDREN:
-		thisAmount += 1.5;
-		if (each.getDaysRented() > 3)
-			thisAmount += (each.getDaysRented() - 3) * 1.5;
+		result += 1.5;
+		if (aRental.getDaysRented() > 3)
+			result += (aRental.getDaysRented() - 3) * 1.5;
 		break;
 	}
-	return thisAmount;
+	return result;
 }
